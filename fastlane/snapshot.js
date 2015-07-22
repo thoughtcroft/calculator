42135a0611bf86f1464f4736a3dbb38844bfc35a
Warren Bain <warren@thoughtcroft.com>
2015-07-22 18:01:04 +1000

    Updated screenshots
    * new snapshot script to also generate landscape versions
    * ran script and generated new screenshots

--- fastlane/snapshot.js
+++ fastlane/snapshot.js
@@ -6,4 +6,9 @@ var window = app.mainWindow();
 
 
 target.delay(3)
+
+target.setDeviceOrientation(UIA_DEVICE_ORIENTATION_PORTRAIT);
 captureLocalizedScreenshot("0-LandingScreen")
+
+target.setDeviceOrientation(UIA_DEVICE_ORIENTATION_LANDSCAPELEFT);
+captureLocalizedScreenshot("1-LandingScreenSideways")
