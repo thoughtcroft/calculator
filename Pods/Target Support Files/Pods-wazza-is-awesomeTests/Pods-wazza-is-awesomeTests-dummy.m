a3358634ac4f46ecbe6e7a75c072504e0b632ed4
Warren Bain <warren@thoughtcroft.com>
2015-07-23 10:22:29 +1000

    Add Pod files

---
+++ Pods/Target Support Files/Pods-wazza-is-awesomeTests/Pods-wazza-is-awesomeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wazza_is_awesomeTests : NSObject
+@end
+@implementation PodsDummy_Pods_wazza_is_awesomeTests
+@end
---
+++ Pods/Target Support Files/Pods-wazza-is-awesomeTests/Pods-wazza-is-awesomeTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_wazza_is_awesomeTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_wazza_is_awesomeTestsVersionString[];
+
