98820f7c8fd9a91bd297390a5bba60f27a38f643
Warren Bain <warren@thoughtcroft.com>
2015-07-27 13:10:19 +1000

    Updated .gitconfig file - removed targets

--- Pods/Target Support Files/Pods-wazza-is-awesomeTests/Pods-wazza-is-awesomeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_wazza_is_awesomeTests : NSObject
-@end
-@implementation PodsDummy_Pods_wazza_is_awesomeTests
-@end
--- Pods/Target Support Files/Pods-wazza-is-awesomeTests/Pods-wazza-is-awesomeTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_wazza_is_awesomeTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_wazza_is_awesomeTestsVersionString[];
-
